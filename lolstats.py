cf9eceadbca0cd450b0db353d68d620d80c4bbdb
Kaffeefilter <thoutrou@gmail.com>
2021-07-09 16:50:43 +0200

    match-v5 fully implemented

--- lolstats.py
+++ lolstats.py
@@ -119,11 +119,10 @@ def getNGames(n):
     summoner = call_riot("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonername)
     me = json.loads(summoner.text)
 
-    runeslookup = generateRunesLookup()
-    shardslookup = generateShardLookup()
+    #runeslookup = generateRunesLookup()
+    #shardslookup = generateShardLookup()
 
     #get a list of the last n matches played
-    #n = 100
     matches = []
     indexes = list(range(0, n, 100))
     indexes.append(n)
@@ -137,7 +136,6 @@ def getNGames(n):
     #get list of ids of relevant gamemodes
     r = requests.get("https://static.developer.riotgames.com/docs/lol/queues.json")
     queues = json.loads(r.text)
-    #gamefilter = [ queue["queueId"] for queue in queues if queue["notes"] == None if queue["description"] != None if "5v5" in queue["description"] ]
     gamefilter = [ queue["queueId"] for queue in queues if queue["notes"] == None if queue["description"] != None if "5v5" in queue["description"] or "Clash" in queue["description"] ]
 
     bar = Bar('Processing', max=n)
@@ -153,18 +151,10 @@ def getNGames(n):
             bar.next()
             continue     
 
-        #get match timeline
-        r = call_riot(f"https://{regionv5}.api.riotgames.com/lol/match/v5/matches/{match}/timeline")
-        matchTimeline = json.loads(r.text)
-        
-        #save last entry for debug purposes
-        """ f = open("data/last_match.json", "w")
-        f.write(json.dumps(matchdetails, indent=4))
-        f.close()
-
-        f = open("data/last_timeline.json", "w")
-        f.write(json.dumps(matchTimeline, indent=4))
-        f.close() """
+        #get match timeline for summonersrift games (not aram)
+        if matchdetails["info"]["gameMode"] == "CLASSIC":
+            r = call_riot(f"https://{regionv5}.api.riotgames.com/lol/match/v5/matches/{match}/timeline")
+            matchTimeline = json.loads(r.text)
 
         #get the right stats
         for participant in matchdetails["info"]["participants"]:
@@ -190,17 +180,18 @@ def getNGames(n):
             ):
                 opponentMatchStats = participant
 
-        #get timeline id for summoner and opponent
-        for participant in matchTimeline["info"]["participants"]:
-            if participant["puuid"] == summonerMatchStats["puuid"]:
-                summonerTimelineId = participant["participantId"]
-            if participant["puuid"] == opponentMatchStats["puuid"]:
-                opponentTimelineId = participant["participantId"]
-
         #get timelinestats
         summonerTimeline = []
         opponentTimeline = []
         if matchdetails["info"]["gameMode"] == "CLASSIC":
+
+            #get timeline id for summoner and opponent
+            for participant in matchTimeline["info"]["participants"]:
+                if participant["puuid"] == summonerMatchStats["puuid"]:
+                    summonerTimelineId = participant["participantId"]
+                if participant["puuid"] == opponentMatchStats["puuid"]:
+                    opponentTimelineId = participant["participantId"]
+
             for frame in matchTimeline["info"]["frames"]:
                 summonerTimeline.append({
                     "cs": frame["participantFrames"][str(summonerTimelineId)]["minionsKilled"] + frame["participantFrames"][str(summonerTimelineId)]["jungleMinionsKilled"],
@@ -225,98 +216,60 @@ def getNGames(n):
             else: 
                 opponents.append({"puuid": participant["puuid"], "name": participant["summonerName"]})
 
-        #for debugging
-        tempdbentry = {
-            "gameMode": matchdetails["info"]["gameMode"],
-            "summonerMatchStats": summonerMatchStats,
-            "opponentMatchStats": opponentMatchStats,
-            "teamStats": teamStats,
-            "teamKills": teamKills,
-            "summonerTimelineId": summonerTimelineId,
-            "opponentTimelineId": opponentTimelineId,
-            "summonerTimeline": summonerTimeline,
-            "opponentTimeline": opponentTimeline,
-            "teammates": teammates,
-            "opponents":opponents
-        }
-        db.append(tempdbentry)
-
-        #TODO here
-        bar.next()
-        continue
-
         dbentry = {
             "game": {
                 "championId": summonerMatchStats["championId"],
                 "teamId": summonerMatchStats["teamId"],
-                "queueId": matchdetails["queueId"],
-                "gameMode": matchdetails["gameMode"],
-                "win": summonerMatchStats["stats"]["win"],
-                "gameDuration": matchdetails["gameDuration"],
-                "role": summonerMatchStats["timeline"]["role"],
-                "lane": summonerMatchStats["timeline"]["lane"]
+                "queueId": matchdetails["info"]["queueId"],
+                "gameMode": matchdetails["info"]["gameMode"],
+                "matchId": matchdetails["metadata"]["matchId"],
+                "win": summonerMatchStats["win"],
+                "gameDuration": matchdetails["info"]["gameDuration"],
+                "individualPosition": summonerMatchStats["individualPosition"],
+                "lane": summonerMatchStats["lane"]
             },
             "stats": {
                 "kda": {
-                    "kills": summonerMatchStats["stats"]["kills"],
-                    "deaths": summonerMatchStats["stats"]["deaths"],
-                    "assists": summonerMatchStats["stats"]["assists"],
-                    "double": summonerMatchStats["stats"]["doubleKills"],
-                    "triple": summonerMatchStats["stats"]["tripleKills"],
-                    "quadra": summonerMatchStats["stats"]["quadraKills"],
-                    "penta": summonerMatchStats["stats"]["pentaKills"],
-                    "unreal": summonerMatchStats["stats"]["unrealKills"],
+                    "kills": summonerMatchStats["kills"],
+                    "deaths": summonerMatchStats["deaths"],
+                    "assists": summonerMatchStats["assists"],
+                    "double": summonerMatchStats["doubleKills"],
+                    "triple": summonerMatchStats["tripleKills"],
+                    "quadra": summonerMatchStats["quadraKills"],
+                    "penta": summonerMatchStats["pentaKills"],
+                    "unreal": summonerMatchStats["unrealKills"],
                     "teamKills": teamKills
                 },
                 "damage": {
-                    "total": summonerMatchStats["stats"]["totalDamageDealtToChampions"],
-                    "magic": summonerMatchStats["stats"]["magicDamageDealtToChampions"],
-                    "physical": summonerMatchStats["stats"]["physicalDamageDealtToChampions"],
-                    "true": summonerMatchStats["stats"]["trueDamageDealtToChampions"],
-                    "selfMitigated": summonerMatchStats["stats"]["damageSelfMitigated"],
-                    "towerdamage": summonerMatchStats["stats"]["damageDealtToTurrets"]
+                    "total": summonerMatchStats["totalDamageDealtToChampions"],
+                    "magic": summonerMatchStats["magicDamageDealtToChampions"],
+                    "physical": summonerMatchStats["physicalDamageDealtToChampions"],
+                    "true": summonerMatchStats["trueDamageDealtToChampions"],
+                    "selfMitigated": summonerMatchStats["damageSelfMitigated"],
+                    "towerdamage": summonerMatchStats["damageDealtToTurrets"]
                 },
-                "ccScore": summonerMatchStats["stats"]["timeCCingOthers"],
-                "deltas": deltas,
-                "csPerMinDeltas": summonerMatchStats["timeline"]["creepsPerMinDeltas"],
-                #"csDiffPerMinDeltas": summonerMatchStats["timeline"]["csDiffPerMinDeltas"] if matchdetails["gameMode"] == "CLASSIC" else None,
-                "totalCsScore": summonerMatchStats["stats"]["totalMinionsKilled"] + summonerMatchStats["stats"]["neutralMinionsKilled"],
-                "totalOpponentCsScore": opponentMatchStats["stats"]["totalMinionsKilled"] + opponentMatchStats["stats"]["neutralMinionsKilled"] if matchdetails["gameMode"] == "CLASSIC" else None,
-                "csDiffAt15": csdiff if matchdetails["gameMode"] == "CLASSIC" else None,
-                "goldPerMinDeltas": summonerMatchStats["timeline"]["goldPerMinDeltas"],
-                #"opponentsGoldPerMinDeltas": opponentMatchStats["timeline"]["goldPerMinDeltas"] if matchdetails["gameMode"] == "CLASSIC" else None,
-                "totalGold": summonerMatchStats["stats"]["goldEarned"],
-                "totalOpponentGold": opponentMatchStats["stats"]["goldEarned"] if matchdetails["gameMode"] == "CLASSIC" else None,
-                "goldDiffAt15": golddiff if matchdetails["gameMode"] == "CLASSIC" else None,
-                "xpPerMinDeltas": summonerMatchStats["timeline"]["xpPerMinDeltas"],
-                #"xpDiffPerMinDeltas": summonerMatchStats["timeline"]["xpDiffPerMinDeltas"] if matchdetails["gameMode"] == "CLASSIC" else None,
-                "xpDiffAt15": xpdiff if matchdetails["gameMode"] == "CLASSIC" else None,
-                "champLevel": summonerMatchStats["stats"]["champLevel"],
-                "opponentChampLevel": opponentMatchStats["stats"]["champLevel"] if matchdetails["gameMode"] == "CLASSIC" else None
+                "ccScore": summonerMatchStats["timeCCingOthers"],
+                "summonerTimeline": summonerTimeline if matchdetails["info"]["gameMode"] == "CLASSIC" else None,
+                "opponentTimeline": opponentTimeline if matchdetails["info"]["gameMode"] == "CLASSIC" else None,
             },
             "teammates": teammates,
             "opponents": opponents,
             "runes": {
-                "keystoneId": summonerMatchStats["stats"]["perk0"],
-                "primary1": summonerMatchStats["stats"]["perk1"],
-                "primary2": summonerMatchStats["stats"]["perk2"],
-                "primary3": summonerMatchStats["stats"]["perk3"],
-                "secondary1": summonerMatchStats["stats"]["perk4"],
-                "secondary2": summonerMatchStats["stats"]["perk5"],
-                "shard1": summonerMatchStats["stats"]["statPerk0"],
-                "shard2": summonerMatchStats["stats"]["statPerk1"],
-                "shard3": summonerMatchStats["stats"]["statPerk2"]
+                "keystoneId": summonerMatchStats["perks"]["styles"][0]["selections"][0]["perk"],
+                "primary1": summonerMatchStats["perks"]["styles"][0]["selections"][1]["perk"],
+                "primary2": summonerMatchStats["perks"]["styles"][0]["selections"][2]["perk"],
+                "primary3": summonerMatchStats["perks"]["styles"][0]["selections"][3]["perk"],
+                "secondary1": summonerMatchStats["perks"]["styles"][1]["selections"][0]["perk"],
+                "secondary2": summonerMatchStats["perks"]["styles"][1]["selections"][1]["perk"],
+                "shard1": summonerMatchStats["perks"]["statPerks"]["offense"],
+                "shard2": summonerMatchStats["perks"]["statPerks"]["flex"],
+                "shard3": summonerMatchStats["perks"]["statPerks"]["defense"]
             }
         }
 
         db.append(dbentry)
         
         bar.next()
-
-        #save last entry for debug purposes
-        """f = open("data/last_entry.json", "w")
-        f.write(json.dumps(dbentry, indent=4))
-        f.close()"""
 
     bar.finish()
     print(f"saved {len(db)} from {len(matches)} games")
